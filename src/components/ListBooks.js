e39b7617f4fd5ce4a12347c91f323433812429f9
Mustafa Kocer <mkocerim@gmail.com>
2022-10-13 07:11:26 +0300

    Loading added

--- src/components/ListBooks.js
+++ src/components/ListBooks.js
@@ -1,26 +1,39 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Loading from "./Loading";
 
 const ListBooks = (props) => {
   const [books, setBooks] = useState(null);
+  const [categories, setCategories] = useState(null);
 
   useEffect(() => {
     // fetch("http://localhost:3004/books",{method:"get "});
     axios
       .get("http://localhost:3004/books")
-      .then((res) => {
-        console.log(res);
-        setBooks(res.data)
+      .then((resBook) => {
+        console.log(resBook);
+        setBooks(resBook.data);
+        axios
+          .get("http://localhost:3004/categories")
+          .then((resCat) => {
+            console.log(resCat);
+            setTimeout(()=>{
+                setCategories(resCat.data);
+
+            },3000)
+          })
+          .catch((errCat) => console.log("Categories catch blog", errCat));
       })
-      .catch((err) => console.log("catch blog",err));
+
+      .catch((errBook) => console.log("Book catch blog", errBook));
   }, []);
 
-  if(books ===null){
-    return(
-        <div>
-            <h1>LOADING...</h1>
-        </div>
-    )
+  if (books === null || categories === null) {
+    return (
+ 
+        <Loading/>
+  
+    );
   }
 
   return (
@@ -29,30 +42,26 @@ const ListBooks = (props) => {
         <thead>
           <tr>
             <th scope="col">Book Name</th>
-            <th scope="col">Author</th>            
+            <th scope="col">Author</th>
             <th scope="col">Category</th>
             <th scope="col">ISBN</th>
-
           </tr>
         </thead>
         <tbody>
-            {
-                books.map((book)=>{
-                    
-                    return(
-                        <tr>
-                        <td>{book.name}</td>
-                        <td>{book.author}</td>
-                        <td>Kategory</td>
-                        <td>{book.isbn}</td>
-                      </tr>
-                    
-                    );
-                })
-            }
-         
-    
-     
+          {books.map((book) => {
+            const category = categories.find(
+              (cat) => cat.id === book.categoryId
+            );
+
+            return (
+              <tr>
+                <td>{book.name}</td>
+                <td>{book.author}</td>
+                <td>{category.name}</td>
+                <td>{book.isbn}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
---
+++ src/components/Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div
+      classNameName="container d-flex justify-content alings-item-center"
+      style={{
+        widows: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+     <button className="btn btn-primary mx-1" type="button" >
+  <span className="spinner-grow spinner-grow-lg"  aria-hidden="true"></span>
+  <span className="visually-hidden">Loading...</span>
+</button>
+<button className="btn btn-primary" type="button" >
+  <span className="spinner-grow spinner-grow-lg"  aria-hidden="true"></span>
+  Loading...
+</button>
+    </div>
+  );
+};
+
+export default Loading;
